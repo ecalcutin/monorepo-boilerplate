12a546e6b9108be267dd59233cd2cebdb5369803
Evgenii Kalkutin <evgheni.calcutin@gmail.com>
2025-05-27 20:05:20 +0300

    feat(): Fix ssr

--- packages/core/src/modules/ssr/utils/render.ts
+++ packages/core/src/modules/ssr/utils/render.ts
@@ -1,18 +1,22 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
-import App from '@packages/web/src/server-entry';
+import renderToString from '@packages/web/src/server-entry';
 
 const render = async () => {
   return new Promise((resolve, reject) => {
     const html = path.resolve(__dirname, './index.html');
+    const rendered = renderToString();
 
     fs.readFile(html, 'utf8', (err, html) => {
       if (err) {
         reject(err);
       }
       resolve(
-        html.replace('<div id="root"></div>', `<div id="root">${App}</div>`),
+        html.replace(
+          '<div id="root"></div>',
+          `<div id="root">${rendered}</div>`,
+        ),
       );
     });
   });
